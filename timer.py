1b8df4b0899bd91b2efcf1bfbaf67d4d9ce738f6
Peter B <31408676+pbelx@users.noreply.github.com>
2018-12-20 22:34:31 +0300

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,17 @@
+import time as t
+
+print "starting\n"
+print "Loading.."
+t.sleep(2)
+print "..."
+print "......"
+t.sleep(2)
+print "Count down to Detonation\n"
+x=10
+while(x>0):
+    print(x)
+    t.sleep(1)
+    x=x-1
+print "*********Kabooooooom!!!!!!!!!!!************\n"
+t.sleep(2)    
+
